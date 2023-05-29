dbfaea2626b9f843f2e3649c375c1229425d52cf
Boomshakal <31717950+Boomshakal@users.noreply.github.com>
2023-05-29 11:29:30 +0800

    添加setting文件

--- fs_app/apps/__init__.py
+++ fs_app/apps/__init__.py
@@ -15,6 +15,8 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from settings import SECRET_KEY, TIMEZONE, DB_CONN
+
 # 实例化SQLAlchemy
 db = SQLAlchemy()
 migrate = Migrate()
@@ -32,15 +34,25 @@ def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
     CORS(app, resources=r'/*')  # 注册CORS, "/*" 允许访问所有api
 
+    app.config['SECRET_KEY'] = SECRET_KEY
+    app.config['SCHEDULER_TIMEZONE'] = TIMEZONE
+    app.config['SCHEDULER_API_ENABLED'] = True
+
     # 初始化App配置 这个app配置就厉害了,专门针对 SQLAlchemy 进行配置
     # SQLALCHEMY_DATABASE_URI 配置 SQLAlchemy 的链接字符串儿
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@127.0.0.1:3306/flask?charset=utf8"
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://%s:%s@%s:%s/%s?charset=utf8" % (
+        DB_CONN["user"], DB_CONN["password"], DB_CONN["host"], DB_CONN["port"], DB_CONN["db"])
 
     # SQLALCHEMY_POOL_SIZE 配置 SQLAlchemy 的连接池大小
-    app.config["SQLALCHEMY_POOL_SIZE"] = 5
+    app.config["SQLALCHEMY_POOL_SIZE"] = 100
+
+    app.config["SQLALCHEMY_POOL_RECYCLE"] = 3600
 
     # SQLALCHEMY_POOL_TIMEOUT 配置 SQLAlchemy 的连接超时时间
     app.config["SQLALCHEMY_POOL_TIMEOUT"] = 15
+
+    app.config["SQLALCHEMY_MAX_OVERFLOW"] = 20
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
---
+++ fs_app/settings.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : settings.py
+# Time       ：2023/5/29 11:10
+# Author     ：李慧敏
+# Description：
+"""
+
+SECRET_KEY = "ea3b6df4f4b58af1c1d8197ca176b6cb"
+TIMEZONE = "Asia/Shanghai"
+
+DB_CONN = {
+    "host": "localhost",
+    "user": "root",
+    "password": "root",
+    "port": 3306,
+    "db": "flask"
+}
